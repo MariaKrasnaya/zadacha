8a733aba5075c63226e0bee42a7d03a1d1e938c9
MariaKrasnaya <mariakrasny@yandex.ru>
2025-04-21 13:01:34 +0300

    т

--- js.js
+++ js.js
@@ -1,10 +1,19 @@
-//Задача
-let originalPrice = 3500;
-let discountPercent = 15;
+//Задача 0
+// let originalPrice = 3500;
+// let discountPercent = 15;
 
-let discountAmount = (originalPrice * discountPercent) / 100;
+// let discountAmount = (originalPrice * discountPercent) / 100;
 
-let finalPrice = originalPrice - discountAmount;
+// let finalPrice = originalPrice - discountAmount;
 
-console.log("Скидка составляет: " + discountAmount + " рублей");
-console.log("Итоговая цена после скидки: " + finalPrice + " рублей");
+// console.log("Скидка составляет: " + discountAmount + " рублей");
+// console.log("Итоговая цена после скидки: " + finalPrice + " рублей");
+
+// Задача 1
+let waterUsed = 12.5;
+
+let pricePerCube = 42.8;
+
+let total = waterUsed * pricePerCube;
+
+console.log("Вы должны заплатить за воду: " + total + " рублей.");
