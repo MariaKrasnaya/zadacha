6b5b7b3babe32ff391f505d49293eaaf021d01e5
MariaKrasnaya <mariakrasny@yandex.ru>
2025-04-28 10:53:37 +0300

    п

--- js.js
+++ js.js
@@ -10,10 +10,21 @@
 // console.log("Итоговая цена после скидки: " + finalPrice + " рублей");
 
 // Задача 1
-let waterUsed = 12.5;
+// let waterUsed = 12.5;
 
-let pricePerCube = 42.8;
+// let pricePerCube = 42.8;
 
-let total = waterUsed * pricePerCube;
+// let total = waterUsed * pricePerCube;
 
-console.log("Вы должны заплатить за воду: " + total + " рублей.");
+// console.log("Вы должны заплатить за воду: " + total + " рублей.");
+
+// Задача 2
+let baseFare = 150;
+
+let pricePerKm = 12;
+
+let distance = 8.3;
+
+let totalFare = baseFare + pricePerKm * distance;
+
+console.log("Итоговая стоимость поездки: " + totalFare + " рублей.");
